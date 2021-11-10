db0f61e79a6f82e6c15e4c48b2c8004735723e90
Stefan <cobrosoffice@gmail.com>
2021-11-10 20:37:01 +0200

    added authentication to course project

--- course_project/recipe-book/src/app/header/header.component.ts
+++ course_project/recipe-book/src/app/header/header.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AuthService } from '../auth/auth.service';
 import { DataStorageService } from '../shared/data-storage.service';
 
 @Component({
@@ -6,15 +8,33 @@ import { DataStorageService } from '../shared/data-storage.service';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent implements OnInit {
-  constructor(private dataStorageService: DataStorageService) {}
+export class HeaderComponent implements OnInit, OnDestroy {
+  isAuthenticated = false;
+  private userSub: Subscription;
+
+  constructor(
+    private dataStorageService: DataStorageService,
+    private authService: AuthService
+  ) {}
   displayDropdown: boolean = false;
 
-  ngOnInit(): void {}
+  ngOnInit() {
+    this.userSub = this.authService.user.subscribe((user) => {
+      this.isAuthenticated = !!user;
+    });
+  }
+
   onSaveData() {
     this.dataStorageService.storeRecipes();
   }
   onFetchData() {
     this.dataStorageService.fetchRecipes().subscribe();
+  }
+  onLogout() {
+    this.authService.logout();
+  }
+
+  ngOnDestroy() {
+    this.userSub.unsubscribe();
   }
 }
---
+++ course_project/recipe-book/src/app/shared/loading-spinner/loading-spinner.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-loading-spinner',
+  template: `<div class="loadingio-spinner-ripple-wkpqeiqnqh">
+    <div class="ldio-uggmonirdl">
+      <div></div>
+      <div></div>
+    </div>
+  </div>`,
+  styleUrls: ['./loading-spinner.component.css'],
+})
+export class LoadingSpinnerComponent {}
